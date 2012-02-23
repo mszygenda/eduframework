6968b04e689dc9ce340c6a2eaaef6f4e4846352f
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2012-02-23 21:51:21 +0100

    simplified routes config

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,11 @@
-module.exports = {
-  routes: function (r) {
-    r.match('/test', 'test#test'); 
-  }
+exports.routes = function (r) {
+  // Enter routing rules here
+  // First rule that matches current request is applied
+  
+  // Example rule:
+  // r.match("/user/:id", "user#show")
+  // routes requests with url user/someId 
+  // invokes action show on UserController
+  // with parameter id set to "someId"
+  r.match('/test', 'test#test'); 
 }
