b045db287ce53defd55b2bba8bbfead285f56c24
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2012-03-18 11:55:17 +0100

    fixed stream error on new version of nodejs

--- app/controllers/test_controller.js
+++ app/controllers/test_controller.js
@@ -1,13 +1,6 @@
 TestController = function() {
   this.test = function() {
-    this.response.write('Parameters: \n');
-    for(param in this.parameters) {
-      if(typeof(this.parameters[param]) == 'function') continue;
-
-      this.response.write(param + " = " + this.parameters[param]);
-      this.response.write('\n');
-    }
-    //this.response.end("Test controller greets you!\n");
+    this.viewModel.msg = "Hello World!";
     this.respond(); 
   }
 }
--- config/core.js
+++ config/core.js
@@ -3,5 +3,6 @@ var root = __dirname + '/../';
 exports.merge({
   rootPath: root,
   viewPath: root + 'app/views',
+  viewModelPath: root + 'app/viewModels',
   controllerPath: root + 'app/controllers',
 })
