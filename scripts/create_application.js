1966d011962c7bfbf811783f225a16b8dc43b307
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2012-04-11 19:40:16 +0200

    added error handling in application generator

--- scripts/create_application.js
+++ scripts/create_application.js
@@ -1,9 +1,28 @@
 var skeleton = require('../lib/skeleton_builder.js');
 
+var handleBuilderError = function (err) {
+    if (err === 'DestinationFolderExists') {
+      console.log("Destination folder already exists");      
+    } else {
+      console.log("Undefined error: " + err.toString());
+    }
+}
+
+var showSuccessInformation = function () {
+  console.log("Application was built successfully");
+  console.log("You can execute it by typing ef-start command in application directory");
+}
+
 var buildApplication = function () {
   parameters = parseParameters(process.argv);
   builder = skeleton.createSkeletonBuilder(parameters.appName);
-  builder.createApplication();
+  try {
+    builder.createApplication();
+    showSuccessInformation(); 
+  }
+  catch (err) {
+    handleBuilderError(err);
+  }
 }
 
 var parseParameters = function (argv) {
