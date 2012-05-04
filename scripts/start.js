cc0bece61fa6488b51b093ff6562218508c19d11
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2012-05-04 23:00:24 +0200

    improved logging format

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,18 @@
+/*jslint node: true, sloppy: true, vars: true, indent: 2 */
+/*global require: false, exports: false, console: false */
+
 var Core = require('../lib/core.js');
 
 var onStart = function () {
-  console.log("ready to accept connections");
-  console.log("listening on port " + Core.HttpEngine.server.port);
+  Core.logger.info("ready to accept connections");
+  Core.logger.info("listening on port " + Core.HttpEngine.server.port);
 }
 
 var onEnd = function () {
-  console.log("shutting down server");
+  Core.logger.info("shutting down server");
 }
+
+console.log("Starting eduFramework server");
 
 Core.initialize(function () {
   Core.HttpEngine.server.on('start', onStart);
